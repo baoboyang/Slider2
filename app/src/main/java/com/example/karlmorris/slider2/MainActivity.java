38c92729d40a43febaf2ee6c512972738519552b
karl <karl@macc.io>
2016-09-06 16:49:44 -0400

    Implemented change listender for slider.

--- app/src/main/java/com/example/karlmorris/slider2/MainActivity.java
+++ app/src/main/java/com/example/karlmorris/slider2/MainActivity.java
@@ -2,17 +2,49 @@ package com.example.karlmorris.slider2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        tv = (TextView) findViewById(R.id.textView);
+        SeekBar sb = (SeekBar) findViewById(R.id.seekBar);
+
+        if (sb != null) {
+            sb.setProgress(100);
+
+            sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    float alphaVal = (float) progress / 100;
+
+
+                    tv.setAlpha(alphaVal);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        }
+
+
 
     }
 }
